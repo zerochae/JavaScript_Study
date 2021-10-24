0fbc460369c0f6b8a8972f1a7209cd85b6bac156
zerochae <zerochae@kakao.com>
2021-10-24 15:47:07 +0900

    #JavaScript Study 1024

--- Study01/js/basicjs07.js
+++ Study01/js/basicjs07.js
@@ -1,6 +1,44 @@
 // document.getElementById('login').addEventListener('click',function(){
 //     document.getElementById('modal').style.display = 'block';
 // });
-document.getElementById('login').addEventListener('click',function(){
-    $('#modal').show();
+// document.getElementById('login').addEventListener('click',function(){
+//     $('#modal').show();
+// });
+
+$('#login').on('click',function(){
+        $('#modal').show();
+});
+
+// $('#nav-sub-button').on('click',function(){
+//     if($('#nav-sub').css('display') == 'block'){
+//         $('#nav-sub').hide();
+//     } else {
+//         $('#nav-sub').show();
+//     }
+// })
+
+$('#nav-sub-button').on('click',function(){
+    $('#nav-sub').slideToggle();
+})
+
+$('#close').on('click',function(){
+    document.getElementById('modal').style.display = 'none';
+});
+
+$('form').on('input',function(e){
+
+    if($('#email').val() == ""){
+        $('#email-alert').show();
+        e.preventDefault();
+    } else {
+        $('#email-alert').hide();
+    }
+    
+    if($('#password').val() == ""){
+        $('#password-alert').show();
+        e.preventDefault();
+    } else {
+        $('#password-alert').hide();
+    }
+    
 });
